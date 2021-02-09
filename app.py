e1abd7a27ea10b1aaaa611a027364cc08a45972b
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2021-02-09 23:36:23 +0000

    Make completed function has been added.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for,request
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://masterDB:qwerty123@cluster0.1efsd.mongodb.net/simpleTodoApp?retryWrites=false&w=majority"
@@ -15,4 +16,9 @@ def index():
 def add_todo():
     new_todo = request.form.get('new-todo')
     todos.insert_one({'task':new_todo, 'completed':False})
+    return redirect(url_for('index'))
+
+@app.route("/complete/<oid>")
+def complete(oid):
+    todo = todos.update_one({'_id': ObjectId(oid)}, {'$set':{'completed':True}})
     return redirect(url_for('index'))
